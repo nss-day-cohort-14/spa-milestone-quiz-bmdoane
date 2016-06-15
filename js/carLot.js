8363ef1d948df9bc151d176f2a0e58521ea1093f
Brian Doane <brianmdoane@gmail.com>
2016-06-15 15:39:49 -0500

    added eventListener.js

--- js/carLot.js
+++ js/carLot.js
@@ -1,17 +1,29 @@
-var CarLot = (function () {
-  var inventory = [];
+var CarLot = ((originalCarLot) => {
+  var inventoryArray = [];
 
-  return {
-    getInventory: function () {
+  return originalCarLot {
+    originalCarLot.getInventory = () => {
 
     },
-    loadInventory: function (callback) {
-      var inventoryLoader = new XMLHttpRequest();
+    originalCarLot.loadInventory = (callback) => {
+      var inventoryRequest = new XMLHttpRequest();
+      // Using the => (fat arrow), requires being explicit with JSON.parse(this.responseText)
+      inventoryRequest.addEventListener(`load`, () => {
+        var inventoryData = JSON.parse(inventoryRequest.responseText);
 
-      inventoryLoader.addEventListener("load", function () {
-
+        inventoryArray = inventoryData.cars;
+        console.log("inventoryArray", inventoryArray);
+        callback(inventoryArray);
       });
+
+      inventoryRequest.addEventListener(`error`, () => {
+        console.log(`An error occurred while transfering the inventoryData`);
+      });
+
+      inventoryRequest.open(`GET`, `inventory.json`);
+      inventoryRequest.send();
     }
+    
   };
 
-})();
+})(CarLot || {});
